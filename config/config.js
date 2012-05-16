f85aea3b6705b06dfd53989eaea02c4dd4d1244b
Andrew Keig <andrew.keig@gmail.com>
2012-05-17 01:26:47 +0200

    Update config/config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var config = {
     title: 'airasoul.',
     welcome: '- welcome to airasoul.',
-    port: 8080,
+    port: 3000,
     view_engine: 'ejs',
     view_path: '/views',
     public_path: '/public',
